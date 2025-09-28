592dba2541c16ea90858d28466566b3b7659f478
Anmeng12 <1283291837@qq.com>
2025-09-28 16:46:32 +0800

    Add files via upload

---
+++ test9.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main()
+{
+	//int num;
+	//
+
+
+	//printf("请输出一个两位整数:");
+	//scanf("%d", &num);
+	//printf("%d\n", num);
+
+
+
+	//int ge = num % 10;
+
+	//int shi = num / 10 % 10;
+	//printf("个位是:%d\n", ge);
+	//printf("十位是:%d\n", shi);
+	//
+	//
+	//printf("%d\n", ge != 7 && shi != 7 && num % 7 != 0);
+
+
+	//printf("%d\n", num % 7 != 0);
+
+	//printf("%d\n", !(shi == 7));
+	//printf("%d", !(ge == 7));
+
+	int a = 1;
+	int b = 5;
+	printf("a=%d,b=%d\n", a, b);
+
+
+
+
+	return 0;
+}
